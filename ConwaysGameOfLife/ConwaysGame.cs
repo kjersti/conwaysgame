d641ea07907c0924cecefcb581798aef1e005409
Kjersti Berg <kjersti.berg@gmail.com>
2012-01-10 13:49:28 +0100

    Initial

---
+++ ConwaysGameOfLife/ConwaysGame.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConwaysGameOfLife
+{
+    internal class ConwaysGame
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+
+    public class GridParser
+    {
+        public Grid Parse(string input)
+        {
+            var lines = input.Split(new[] {Environment.NewLine}, StringSplitOptions.None);
+
+            var dimensions = lines.Skip(1).First().Split(' ').Select(x => Convert.ToInt32(x)).ToList();
+
+            return new Grid(
+                generation: Convert.ToInt32(lines.First().Replace("Generation ", "").Replace(":", "")),
+                cells:
+                    lines.Skip(2).Select(
+                        (line, row) => line.Select((c, col) => new Cell(row, col, c == '*')).Where(c => c.Alive)).
+                    SelectMany(x => x),
+                rowCount: dimensions.First(),
+                colCount: dimensions.Last());
+        }
+    }
+
+    public class Grid
+    {
+        private readonly IEnumerable<Cell> _cells;
+        private readonly int _generation;
+        private readonly int _rowCount;
+        private readonly int _colCount;
+
+        public Grid(int generation, IEnumerable<Cell> cells, int rowCount, int colCount)
+        {
+            _generation = generation;
+            _cells = cells;
+            _rowCount = rowCount;
+            _colCount = colCount;
+        }
+
+        public bool IsAlive(int x, int y)
+        {
+            return _cells.FirstOrDefault(c => c.X == x && c.Y == y).Alive;
+        }
+
+        public Grid Tick()
+        {
+            return new Grid(_generation + 1, NextGeneration(), _rowCount, _colCount);
+        }
+
+        private IEnumerable<Cell> NextGeneration()
+        {
+            return Enumerable.Range(0, _rowCount).
+                Select(row => Enumerable.Range(0, _colCount).
+                                  Where(col => WillLive(row, col)).
+                                  Select(col => new Cell(row, col, true))).SelectMany(x => x);
+        }
+
+        private bool WillLive(int row, int col)
+        {
+            return (!IsAlive(row, col) && CountLiveNeighbours(row, col) == 3)
+                   ||
+                   (IsAlive(row, col) && CountLiveNeighbours(row, col) == 3 || CountLiveNeighbours(row, col) == 2);
+        }
+
+        private int CountLiveNeighbours(int row, int col)
+        {
+            var count = 0;
+            if (IsAlive(row + 1, col)) count++;
+            if (IsAlive(row + 1, col + 1)) count++;
+            if (IsAlive(row + 1, col - 1)) count++;
+            if (IsAlive(row, col + 1)) count++;
+            if (IsAlive(row, col - 1)) count++;
+            if (IsAlive(row - 1, col + 1)) count++;
+            if (IsAlive(row - 1, col - 1)) count++;
+            if (IsAlive(row - 1, col)) count++;
+
+            return count;
+        }
+    }
+
+    public struct Cell
+    {
+        public readonly int X, Y;
+        public readonly bool Alive;
+
+        public Cell(int x, int y, bool alive)
+        {
+            X = x;
+            Y = y;
+            Alive = alive;
+        }
+    }
+}
---
+++ ConwaysGameOfLifeTest/ConwaysGameTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using ConwaysGameOfLife;
+using NUnit.Framework;
+
+namespace ConwaysGameOfLifeTest
+{
+    [TestFixture]
+    public class ConwaysGameTest
+    {
+        [Test]
+        public void CanParseGridFromString()
+        {
+            var input = "Generation 1:" + Environment.NewLine
+                        + "4 8" + Environment.NewLine
+                        + "........" + Environment.NewLine
+                        + "....*..." + Environment.NewLine
+                        + "...**..." + Environment.NewLine
+                        + "........" + Environment.NewLine;
+
+            var actual = new GridParser().Parse(input);
+
+            Assert.That(actual.IsAlive(1, 4));
+            Assert.That(actual.IsAlive(2, 3));
+            Assert.That(actual.IsAlive(2, 4));
+            Assert.IsFalse(actual.IsAlive(0, 0));
+            Assert.IsFalse(actual.IsAlive(1, 0));
+            Assert.IsFalse(actual.IsAlive(3, 0));
+        }
+
+        [Test]
+        public void CanParseSmallGridWithOneLineCell()
+        {
+            var input = "Generation 1:" + Environment.NewLine + "1 1" + Environment.NewLine + "*" + Environment.NewLine;
+
+            Assert.That(new GridParser().Parse(input).IsAlive(0, 0));
+        }
+
+        [Test]
+        public void CanGenerateNextGenerationWithOneCellGrid()
+        {
+            IList<Cell> cells = new List<Cell>(new[] { new Cell(0, 0, true) });            
+            var grid = new Grid(1, cells, 1, 1);
+
+            Assert.IsFalse(grid.Tick().IsAlive(0,0));
+        }
+
+        [Test]
+        public void CanGenerateNextGenerationWithLargerGrid()
+        {
+            IList<Cell> cells = new List<Cell>(new[] { new Cell(1, 4, true), new Cell(2, 3, true), new Cell(2, 4, true)});            
+            var grid = new Grid(1, cells, 4, 8);
+
+            var nextGen = grid.Tick();
+            Assert.That(nextGen.IsAlive(1, 3));
+            Assert.That(nextGen.IsAlive(1, 4));
+            Assert.That(nextGen.IsAlive(2, 3));
+            Assert.That(nextGen.IsAlive(2, 4));
+        }
+
+        [Test]
+        public void CanGenerateNextGenerationWhereCellDies()
+        {
+            IList<Cell> cells = new List<Cell>(new[] { new Cell(0, 0, true)});
+            var grid = new Grid(1, cells, 4, 8);
+
+            Assert.IsFalse(grid.Tick().IsAlive(0, 0));
+        }
+    }
+}
